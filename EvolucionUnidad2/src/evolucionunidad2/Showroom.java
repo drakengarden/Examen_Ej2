97d33f5620216b8a749531f1b26079d29a619b0d
Camilo Aleman <labc205@uam.net.ni>
2022-10-13 09:39:37 -0600

    Diego Garcia Javier Ignacio Camilo Herradora

---
+++ EvolucionUnidad2/src/evolucionunidad2/Showroom.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evolucionunidad2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author labc205
+ */
+public class Showroom {
+
+   
+
+   public int addArray1( Array1 newNumber ) {
+      
+      return myNumbers.add(newNumber);
+   }
+}
